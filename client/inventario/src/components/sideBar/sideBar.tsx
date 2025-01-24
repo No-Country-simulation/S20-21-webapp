0314e9ae143bbbae5e8318d0c3d5294908af8357
Gianella A. Achetoni <gianellaachetoni@gmail.com>
2025-01-24 12:27:54 -0300

    update responsil

--- client/inventario/src/components/sideBar/sideBar.tsx
+++ client/inventario/src/components/sideBar/sideBar.tsx
@@ -1,32 +1,35 @@
 import { HomeIcon, CubeIcon, TruckIcon, ExclamationTriangleIcon } from '@heroicons/react/24/outline';
 import { Link } from 'react-router-dom';  // Importa Link de React Router
 
+interface SideBarProps {
+  closeSidebar: () => void; // Define el tipo de la función de cierre
+}
 
-const SideBar = () => {
+const SideBar: React.FC<SideBarProps> = ({ closeSidebar }) => {
   return (
     <div className="w-full h-full bg-customBlue text-beigeclaro p-5 space-y-4 flex flex-col">
       <h2 className="text-xl font-bold mb-5 mt-3"> INVENTIA </h2>
       <ul className="space-y-2 flex-1">
         <li>
-          <Link to="./home" className="flex items-center py-2 px-4 hover:bg-bluelith rounded">
+          <Link to="./home" className="flex items-center py-2 px-4 hover:bg-bluelith rounded" onClick={closeSidebar}>
             <HomeIcon className="w-6 h-6 mr-3" />
             HOME
           </Link>
         </li>
         <li>
-          <Link to="./inventario" className="flex items-center py-2 px-4 hover:bg-bluelith rounded">
+          <Link to="./inventario" className="flex items-center py-2 px-4 hover:bg-bluelith rounded" onClick={closeSidebar}>
             <CubeIcon className="w-6 h-6 mr-3" />
             INVENTARIO
           </Link>
         </li>
         <li>
-          <Link to="#" className="flex items-center py-2 px-4 hover:bg-bluelith rounded">
+          <Link to="#" className="flex items-center py-2 px-4 hover:bg-bluelith rounded" onClick={closeSidebar}>
             <TruckIcon className="w-6 h-6 mr-3" />
             PROVEDORES
           </Link>
         </li>
         <li>
-          <Link to="#" className="flex items-center py-2 px-4 hover:bg-bluelith rounded">
+          <Link to="#" className="flex items-center py-2 px-4 hover:bg-bluelith rounded" onClick={closeSidebar}>
             <ExclamationTriangleIcon className="w-6 h-6 mr-3" />
             REPORTES
           </Link>
